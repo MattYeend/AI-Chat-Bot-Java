1a0d8cad35372f0b5cb644e39616a6a0fd41a054
MattYeend <mattyeend1@hotmail.co.uk>
2021-07-18 10:06:59 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/aichatbot/ButtonDemo.java
@@ -0,0 +1,48 @@
+/*
+    Author: Matthew Yeend
+    Title: Button Demo
+    Project: AI Chat Bot
+    Date: 18/07/21
+    Version: 1.0
+ */
+package com.mycompany.aichatbot;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+/**
+ *
+ * @author matthewyeend
+ */
+public class ButtonDemo extends Applet implements ActionListener {
+    String msg = "";
+    Button yes, no, maybe;
+    
+    public void init(){
+        yes = new Button("Yes");
+        no = new Button("No");
+        maybe = new Button("Maybe");
+        add(yes);
+        add(no);
+        add(maybe);
+        yes.addActionListener(this);
+        no.addActionListener(this);
+        maybe.addActionListener(this);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        String str = ae.getActionCommand();
+        if(str.equals("Yes")){
+            msg = "You pressed Yes.";
+        } else if(str.equals("No")){
+            msg = "You pressed No.";
+        } else {
+            msg = "You pressed Maybe.";
+        }
+    }
+    
+    public void paint(Graphics g){
+        g.drawString(msg, 6, 100);
+    }
+}
